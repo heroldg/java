99a5e3e236a4d91ab40851104f9477d068b0cc38
heroldg <80465743+heroldg@users.noreply.github.com>
2022-08-10 17:50:41 +0200

    moyenneDeNotesv4 updates

---
+++ testJava/src/test/jour11/DoubleMath.java
@@ -0,0 +1,5 @@
+package test.jour11;
+
+public class DoubleMath {
+
+}
---
+++ testJava/src/test/jour11/moyenneDeNotesv4.java
@@ -0,0 +1,39 @@
+package test.jour11;
+
+import java.util.Scanner;
+
+public class moyenneDeNotesv4 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        final int MAX = 10;
+        int[] TableauNotes = new int[MAX];
+        int nbNotes = 0;
+        int note;
+        int somme = 0;
+
+        do {
+            System.out.println("Notes ? [-1 pour mettre fin]");
+            note = console.nextInt();
+            if (note != -1) {
+                TableauNotes[nbNotes] = note;
+                nbNotes++;
+
+            }
+        } while (note != -1);
+        if (nbNotes == MAX) {
+            System.out.println("Une moyen de plus de 10 000 notes... maximum atteint");
+        } else {
+            System.out.print("La moyen des notes:");
+            for (int i = 0; i < nbNotes; i++) {
+                System.out.print(TableauNotes[i] + "; ");
+                somme += TableauNotes[i];
+            }
+
+            System.out.println();
+            System.out.println("Total(" + somme + ") divisé par le Nombres de notes (" + nbNotes + ")");
+            System.out.println("Moyenne :" + somme / nbNotes);
+
+        }
+
+    }
+}
