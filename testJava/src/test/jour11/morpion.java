9a8ce84854f42ffe8393416cd0d0e0d87d7f9c39
heroldg <80465743+heroldg@users.noreply.github.com>
2023-01-05 19:35:11 +0100

    morpion final qui peut être optimiser (les condition)

--- testJava/src/test/jour11/morpion.java
+++ testJava/src/test/jour11/morpion.java
@@ -5,15 +5,27 @@ import java.util.Scanner;
 public class morpion {
     public static void main(String[] args) {
 
+        // Variables constantes pour les colonnes et lignes d jeu morpion
         final int COLONNES = 3;
         final int LIGNES = 3;
+
+        // Caractères de base quand le tableau ne sera pas encore remplis
         char options = '?';
+
+        // Le nombre de coup joué lors de la partie
         int nbCoupDeJeu = 0;
 
+        // Les symboles des 2 joueur: X et O
+        char playerO = 'O';
+        char playerX = 'X';
+
+        // Préparation de la console pour que les joueur puissent rentrer des valeurs
         Scanner console = new Scanner(System.in);
 
+        // Création du tableau qui répresente l'espace de jeu du morpion
         char[][] morpion = new char[COLONNES][LIGNES];
 
+        // Prèsentation du tableau
         for (int i = 0; i < LIGNES; i++) {
             System.out.print("|");
             for (int j = 0; j < COLONNES; j++) {
@@ -26,94 +38,93 @@ public class morpion {
 
         }
 
-        System.out.println();
-        System.out.println();
+        // Début de la boucle while permettent de jouer plusieurs coups d'affilié, 9
+        // coups pour 9 cases.
+        while (nbCoupDeJeu < 9) {
 
-        char playerO = 'O';
-        char playerX = 'X';
+            if (morpion[0][0] == playerO && morpion[0][1] == playerO && morpion[0][2] == playerO
+                    || morpion[1][0] == playerO && morpion[1][1] == playerO && morpion[1][2] == playerO
+                    || morpion[2][0] == playerO && morpion[2][1] == playerO && morpion[2][2] == playerO
+                    || morpion[0][0] == playerO && morpion[1][0] == playerO && morpion[2][0] == playerO
+                    || morpion[0][1] == playerO && morpion[1][1] == playerO && morpion[2][1] == playerO
+                    || morpion[0][2] == playerO && morpion[1][2] == playerO && morpion[2][2] == playerO
+                    || morpion[0][0] == playerO && morpion[1][1] == playerO && morpion[2][2] == playerO
+                    || morpion[2][0] == playerO && morpion[1][1] == playerO && morpion[0][2] == playerO) {
+                System.out.println("Le Joueur O à gagné la partie");
+                nbCoupDeJeu = 9;
+            } else if (morpion[0][0] == playerX && morpion[0][1] == playerX && morpion[0][2] == playerX
+                    || morpion[1][0] == playerX && morpion[1][1] == playerX && morpion[1][2] == playerX
+                    || morpion[2][0] == playerX && morpion[2][1] == playerX && morpion[2][2] == playerX
+                    || morpion[0][0] == playerX && morpion[1][0] == playerX && morpion[2][0] == playerX
+                    || morpion[0][1] == playerX && morpion[1][1] == playerX && morpion[2][1] == playerX
+                    || morpion[0][2] == playerX && morpion[1][2] == playerX && morpion[2][2] == playerX
+                    || morpion[0][0] == playerX && morpion[1][1] == playerX && morpion[2][2] == playerX
+                    || morpion[2][0] == playerX && morpion[1][1] == playerX && morpion[0][2] == playerX) {
+                System.out.println("Le Joueur O à gagné la partie");
+                nbCoupDeJeu = 9;
 
-        System.out.println("PLAYER O: Quelles colonnes ?");
-        int morpionColonnes = console.nextInt();
-        morpionColonnes = morpionColonnes - 1;
+            } else {
 
-        System.out.println();
+                // Choix colonnes pour les deux joueurs
+                if (nbCoupDeJeu % 2 == 0) {
+                    System.out.println("PLAYER O: Quelles colonnes ?");
+                } else {
 
-        System.out.println("PLAYER O: Quelles lignes ?");
-        int morpionlignes = console.nextInt();
-        morpionlignes = morpionlignes - 1;
+                    System.out.println("PLAYER X: Quelles colonnes ?");
 
-        morpion[morpionColonnes][morpionlignes] = playerO;
+                }
+                int morpionColonnes = console.nextInt();
+                morpionColonnes = morpionColonnes - 1;
 
-        for (int i = 0; i < LIGNES; i++) {
+                // Choix lignes pour les deux joueurs
+                if (nbCoupDeJeu % 2 == 0) {
 
-            System.out.print("|");
-            for (int j = 0; j < COLONNES; j++) {
-                // System.out.print(i);
-                // System.out.print(j);
+                    System.out.println("PLAYER O: Quelles lignes ?");
+                } else {
 
-                System.out.print(morpion[j][i]);
-                System.out.print("|");
+                    System.out.println("PLAYER X: Quelles lignes ?");
+                }
+                int morpionlignes = console.nextInt();
+                morpionlignes = morpionlignes - 1;
 
+                // changement de tours pour les 2 participant crée en finction de valeurs pair
+                // et impair
+                if (nbCoupDeJeu % 2 == 0) {
+
+                    if (morpion[morpionColonnes][morpionlignes] == playerX) {
+                        System.out.println("Cette case des déjà prise, passez votre tour");
+                    } else {
+                        morpion[morpionColonnes][morpionlignes] = playerO;
+                        nbCoupDeJeu++;
+                    }
+
+                } else if (nbCoupDeJeu % 2 == 1) {
+                    if (morpion[morpionColonnes][morpionlignes] == playerO) {
+                        System.out.println("Cette case des déjà prise, passez votre tour");
+                    } else {
+
+                        morpion[morpionColonnes][morpionlignes] = playerX;
+                        nbCoupDeJeu++;
+                    }
+
+                }
+
+                for (int i = 0; i < LIGNES; i++) {
+
+                    System.out.print("|");
+                    for (int j = 0; j < COLONNES; j++) {
+                        System.out.print(morpion[j][i]);
+                        System.out.print("|");
+
+                    }
+                    System.out.println();
+                }
+                // System.out.println(nbCoupDeJeu);
             }
-            System.out.println(
-
-            );
         }
+        System.out.print("Fin du jeu");
 
-        System.out.println("PLAYER O: Quelles colonnes ?");
-        morpionColonnes = console.nextInt();
-        morpionColonnes = morpionColonnes - 1;
-
-        System.out.println();
-
-        System.out.println("PLAYER O: Quelles lignes ?");
-        morpionlignes = console.nextInt();
-        morpionlignes = morpionlignes - 1;
-
-        morpion[morpionColonnes][morpionlignes] = playerO;
-
-        for (int i = 0; i < LIGNES; i++) {
-
-            System.out.print("|");
-            for (int j = 0; j < COLONNES; j++) {
-                // System.out.print(i);
-                // System.out.print(j);
-
-                System.out.print(morpion[j][i]);
-                System.out.print("|");
-
-            }
-            System.out.println(
-
-            );
-        }
-        System.out.println("PLAYER X: Quelles colonnes ?");
-        morpionColonnes = console.nextInt();
-        morpionColonnes = morpionColonnes - 1;
-
-        System.out.println();
-
-        System.out.println("PLAYER X: Quelles lignes ?");
-        morpionlignes = console.nextInt();
-        morpionlignes = morpionlignes - 1;
-
-        morpion[morpionColonnes][morpionlignes] = playerX;
-
-        for (int i = 0; i < LIGNES; i++) {
-
-            System.out.print("|");
-            for (int j = 0; j < COLONNES; j++) {
-                // System.out.print(i);
-                // System.out.print(j);
-
-                System.out.print(morpion[j][i]);
-                System.out.print("|");
-
-            }
-            System.out.println(
-
-            );
-        }
-
+        console.close();
     }
+
 }
